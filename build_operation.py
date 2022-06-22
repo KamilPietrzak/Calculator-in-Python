089e6edf5643d2a6b1dba35a1c5c11077a2231e8
kamilpietrzakpl <kamilpietrzkapl@gmail.com>
2022-06-22 23:30:19 +0200

    Rebuild class BildOperation. Clearing the file main.py and gui_load.py.

--- build_operation.py
+++ build_operation.py
@@ -1,14 +1,42 @@
 class BuildOperation:
 
-    def __init__(self, operation=""):
+    def __init__(self, operation="0"):
         self.operation=operation
-        #self.math_signs=["+","-",chr(215),chr(247)]
+        self.special_char=["+","-",chr(215),chr(247), ",",")","("]
 
-    #def __checkBrackets(self):
-    #    return True if self.operation.count("(")==self.operation.count(")") else False
+    def __len__(self):
+        return len(self.operation)
+
+    def __is_integer(self, char):
+        try:
+            int(char)
+        except ValueError:
+            return False
+        else:
+            return True
+
+    def __checkBrackets(self):                                                              # True==Good / False==Bad
+        if self.operation.count("(")==self.operation.count(")"):
+            return False, "{} razy otowrzyłeś nawias, a zamknołeś {} razy. Ilość otwrać i zamknięc nawiasów musi być taka sama.".format(str(self.operation.count("(")),str(self.operation.count(")")))
+        elif "()" in self.operation:
+            return False, "Wy działaniu znajduje się pusty nawias."
+        else:
+            return True
+
+    def __checkChar(self, char):                                                            # True==Good / False==Bad
+        if (char in self.special_char and self.operation[-1] in ["+","-",chr(215),chr(247), ","]) or (char in ["+",chr(215),chr(247),","] and self.operation[-1]=="("):
+            return False
+        else:
+            return True
 
     def addChar(self, char):
-        self.operation+=str(char)
+        if char=="(" and self.__len__()==1:
+            self.operation=self.operation.replace("0","(")
+        else:
+            if self.__checkChar(char):
+                self.operation+=str(char)
+            else:
+                self.operation=self.operation[0:-1]+str(char)
 
     def delChar(self):
         self.operation=self.operation[0:-1]
@@ -16,6 +44,9 @@ class BuildOperation:
     def clean(self):
         self.operation=""
 
-    def returnEndValue(self):
+    def __repr__(self):
         return self.operation.replace(chr(247),"/").replace(chr(215),"*").replace(",",".")
+
+    def __str__(self):
+        return self.operation
 
